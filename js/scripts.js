01c1db8401ab5248a2d284754de4bc08f7a64e07
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-28 17:17:47 -0300

    terminando hexadecimal

--- js/scripts.js
+++ js/scripts.js
@@ -10,32 +10,9 @@ const elementoHexadecimal = $.querySelector("#hexadecimal")
 // BTN - gerar
 const btnGerar = $.querySelector("#btn-gerar")
 
-// Gerando número aleatório
-const gerandoNumeroAleatorio = () => {
-    const min = Math.ceil(0)
-    const max = Math.floor(9)
-    const numero = Math.floor(Math.random() * (max - min + 1)) + min
-    return numero
-}
-
-// Gerando letras aleatórias
-const gerandoLetrasAleatorias = () => {
-    const letras = ["a", "b", "c", "d", "e", "f"]
-    const minIndice = Math.ceil(0)
-    const maxIndice = Math.floor(5)
-    const numeroIndice = Math.floor(Math.random() * (maxIndice - minIndice + 1)) + minIndice
-    return letras[numeroIndice]
-}
-
 // Gerando hexadecimal
 const gerandoHexadecimal = () => {
-    let hexadecimal = "#"
-    const numeroAleatorio = gerandoNumeroAleatorio()
-    const letraAleatoria = gerandoLetrasAleatorias()
-
-    for (let i = 0; i < 3; i++){
-        hexadecimal += `${numeroAleatorio}`
-    }
+    const hexadecimal = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
 
     return hexadecimal
 }
